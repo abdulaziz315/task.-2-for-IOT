780e02b2aa3d316e7232226c66b7dd590e338b46
abdulaziz315 <108241230+abdulaziz315@users.noreply.github.com>
2022-08-10 02:35:21 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+let input = document.querySelector(".inputBx input"),
+ btn = document.querySelector(".inputBx icon");
+ icon = document.querySelector(".inputBx icon i");
+
+ let SpeechRecognition = window.SpeechRecognition ||window. webkitSpeechRecognition;
+ if (SpeechRecognition)
+ {
+    console.log("supported");
+   let recognition = new SpeechRecognition();
+  //console.log(recognition);
+  btn.addEventListener("click",()=>{
+    if (icon.classList.contains('fa-microphone'))
+{
+    
+    recognition.start();
+}
+else
+{
+    
+    recognition.stop()
+}
+})
+recognition.addEventListener("start",()=>{
+    icon.classlist.replace("fa-microphone","fa-microphone-slash")
+})
+recognition.addEventListener("end",()=>{
+    icon.classlist.replace("fa-microphone-slash","fa-microphone")
+})
+recognition.addEventListener("result",(event)=>{
+    //console.log("event");
+    let transcript =event.results[0][0].transcript;
+   input.value = transcript;
+})
+ }
+ else
+ {
+    console.log("not supported");
+ }
